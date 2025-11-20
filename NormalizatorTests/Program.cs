89589305982681e677092c28d8b53603394b9067
Patryk Srzedziński <Patryk.Srzedzinski@kruksa.pl>
2025-11-20 11:09:34 +0100

    NormalizatorTests

---
+++ NormalizatorTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using NormalizatorTests;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var originalFilePath = config["OriginalFilePath"];
+var resultFilePath = config["ResultFilePath"];
+var apiUrl = config["ApiUrl"];
+
+TestEngine.RunTest(originalFilePath!, resultFilePath!, apiUrl!);
---
+++ NormalizatorTests/TestEngine.cs
@@ -0,0 +1,221 @@
+﻿using ClosedXML.Excel;
+using System.Net.Http.Json;
+
+namespace NormalizatorTests
+{
+    internal class TestEngine
+    {
+        public static void RunTest(string originalFilePath, string resultFilePath, string apiUrl)
+        {
+            using var workbook = new XLWorkbook(originalFilePath);
+            var sheet = workbook.Worksheet(1);
+
+            AddResultColumns(sheet);
+
+            var indexes = GetColumnIndexes(sheet);
+            var rowsNo = sheet.Rows().Count();
+         
+            for (int i = 2; i <= rowsNo; i++)
+            {
+                var result = GetResultForRow(sheet, i, indexes, apiUrl);
+                WriteRowResult(sheet, i, indexes, result);
+                SetBackroundColor(sheet, i);
+            }
+
+            workbook.SaveAs(resultFilePath);
+        }
+
+        private static void SetBackroundColor(IXLWorksheet sheet, int rowNo)
+        {
+            var columnsNo = sheet.Columns().Count();
+
+            for (int i = columnsNo; i > 0; i--)
+            {
+                var header = sheet.Cell(1, i).Value.ToString();
+                if (header.Contains("EXPECTED"))
+                {
+                    var expectedValue = sheet.Cell(rowNo, i).Value.ToString();
+                    if (expectedValue == "null")
+                        expectedValue = string.Empty;
+
+                    var resultValue = sheet.Cell(rowNo, i + 1).Value.ToString();
+                    if (resultValue == "null")
+                        resultValue = string.Empty;
+
+                    var color = XLColor.Red;
+
+                    if (string.Equals(expectedValue, resultValue, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        color = XLColor.Green;
+                    }
+
+                    sheet.Cell(rowNo, i + 1).Style.Fill.BackgroundColor = color;
+                }
+            }
+        }
+
+        private static void WriteRowResult(IXLWorksheet sheet, int rowNo, ColumnIndexes idx, NormalizationApiResponseDto result)
+        {
+            sheet.Cell(rowNo, idx.ResultBuildingNoIndex).Value = result.BuildingNumber;
+            sheet.Cell(rowNo, idx.ResultCityIndex).Value = result.City;
+            sheet.Cell(rowNo, idx.ResultCommuneIndex).Value = result.Commune;
+            sheet.Cell(rowNo, idx.ResultDistrictIndex).Value = result.District;
+            sheet.Cell(rowNo, idx.ResultPostalCodeIndex).Value = result.PostalCode;
+            sheet.Cell(rowNo, idx.ResultProvinceIndex).Value = result.Province;
+            sheet.Cell(rowNo, idx.ResultStreetIndex).Value = $"{result.StreetPrefix} {result.StreetName}"?.Trim(); ;
+        }
+
+        private static NormalizationApiResponseDto GetResultForRow(IXLWorksheet sheet, int rowNo, ColumnIndexes idx, string endpoint)
+        {
+            try
+            {
+                var streetName = sheet.Cell(rowNo, idx.RequestStreetIndex).Value.ToString();
+                var buildingNo = sheet.Cell(rowNo, idx.RequestBuildingNoIndex).Value.ToString();
+                var city = sheet.Cell(rowNo, idx.RequestCityIndex).Value.ToString();
+                var postalCode = sheet.Cell(rowNo, idx.RequestPostalCodeIndex).Value.ToString();
+
+                if (streetName == "null")
+                    streetName = string.Empty;
+                if (buildingNo == "null")
+                    buildingNo = string.Empty;
+                if (city == "null")
+                    city = string.Empty;
+                if (postalCode == "null")
+                    postalCode = string.Empty;
+
+                var body = new
+                {
+                    StreetName = streetName,
+                    StreetPrefix = string.Empty,
+                    BuildingNumber = buildingNo,
+                    City = city,
+                    PostalCode = postalCode
+                };
+
+                using (var client = new HttpClient())
+                {
+                    var response = client.PostAsJsonAsync(endpoint, body).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return new NormalizationApiResponseDto();
+                    }
+
+                    var responseObject = response.Content.ReadFromJsonAsync<NormalizationApiResponseDto>().Result;
+                    if (responseObject is null)
+                    {
+                        return new NormalizationApiResponseDto();
+                    }
+
+                    return responseObject;
+                }
+            }
+            catch
+            {
+                return new NormalizationApiResponseDto();
+            }
+        }
+
+        private static void AddResultColumns(IXLWorksheet sheet)
+        {
+            var columnsNo = sheet.Columns().Count();
+
+            for (int i = columnsNo; i > 0; i--)
+            {
+                var header = sheet.Cell(1, i).Value.ToString();
+                if (header.Contains("EXPECTED"))
+                {
+                    sheet.Column(i).InsertColumnsAfter(1);
+                    sheet.Cell(1, i + 1).Value = header.Replace("EXPECTED", "RESULT");
+                }
+            }
+        }
+
+        private static ColumnIndexes GetColumnIndexes(IXLWorksheet sheet)
+        {
+            var result = new ColumnIndexes();
+
+            var columnsNo = sheet.Columns().Count();
+
+            for (int i = columnsNo; i > 0; i--)
+            {
+                var header = sheet.Cell(1, i).Value.ToString();
+
+                if (header.Contains("REQUEST"))
+                {
+                    if (header.Contains("street"))
+                        result.RequestStreetIndex = i;
+                    if (header.Contains("building"))
+                        result.RequestBuildingNoIndex = i;
+                    if (header.Contains("city"))
+                        result.RequestCityIndex = i;
+                    if (header.Contains("postal"))
+                        result.RequestPostalCodeIndex = i;
+                }
+
+                if (header.Contains("RESULT"))
+                {
+                    if (header.Contains("street"))
+                        result.ResultStreetIndex = i;
+                    if (header.Contains("building"))
+                        result.ResultBuildingNoIndex = i;
+                    if (header.Contains("city"))
+                        result.ResultCityIndex = i;
+                    if (header.Contains("postal"))
+                        result.ResultPostalCodeIndex = i;
+                    if (header.Contains("commune"))
+                        result.ResultCommuneIndex = i;
+                    if (header.Contains("district"))
+                        result.ResultDistrictIndex = i;
+                    if (header.Contains("province"))
+                        result.ResultProvinceIndex = i;
+                }
+            }
+
+            return result;
+        }
+
+        private class ColumnIndexes
+        {
+            public int RequestStreetIndex { get; set; }
+            public int RequestBuildingNoIndex { get; set; }
+            public int RequestCityIndex { get; set; }
+            public int RequestPostalCodeIndex { get; set; }
+            public int ResultStreetIndex { get; set; }
+            public int ResultBuildingNoIndex { get; set; }
+            public int ResultCityIndex { get; set; }
+            public int ResultPostalCodeIndex { get; set; }
+            public int ResultProvinceIndex { get; set; }
+            public int ResultDistrictIndex { get; set; }
+            public int ResultCommuneIndex{ get; set; }
+        }
+
+        private class NormalizationApiResponseDto
+        {
+            public bool IsMultiFamily { get; set; }
+
+            public decimal? Longitude { get; set; }
+
+            public decimal? Latitude { get; set; }
+
+            public string? PostOfficeLocation { get; set; }
+
+            public decimal CombinedProbability { get; set; }
+
+            public string? Commune { get; set; }
+
+            public string? Province { get; set; }
+
+            public string? District { get; set; }
+
+            public string? StreetName { get; set; }
+
+            public string? BuildingNumber { get; set; }
+
+            public string? City { get; set; }
+
+            public string? PostalCode { get; set; }
+
+            public string? StreetPrefix { get; set; }
+        }
+    }
+}
